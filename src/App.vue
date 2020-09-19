a28c76cd50305f704c9d231ea2e67a4c8a6f117d
Chme <meng.chen@aiseeding.com>
2020-09-19 13:50:39 +0800

    更新项目

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,23 @@
 <template>
   <div id="app">
-<!--    <img alt="Vue logo" src="./assets/logo.png">-->
-    <transition name="fade" key="0">
-      <HelloWorld msg="Welcome to Your Vue.js App" v-show="show"></HelloWorld>
-    </transition>
-    <ChenMe @switchShow="switchShow">
-    </ChenMe>
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import ChenMe from './components/ChenMe.vue';
-
 export default {
-  name: 'App',
-  data(){
-    return {
-      msg:'hello',
-      show:true
-    }
-  },
-  components: {
-    HelloWorld,
-    ChenMe
-  },
-  methods:{
-    switchShow(){
-      this.show=!this.show
-    }
-  }
+  name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  font-size: 12px;
 }
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 1s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-
 </style>
